39c6040e1ed2b3bf5bca710289a3604fcc388166
deryana <derana765@gmail.com>
2024-07-10 08:59:31 +0700

    Update lagi ges -d

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # desk-hololive-id
+
+Projek Pertama Gw Di React Yaitu berupa sebuah website yang menampilkan beberapa Talent Hololive ID dengan sound deskripsi dan bahkan orang nya
--- src/components/container.js
+++ src/components/container.js
@@ -3,11 +3,16 @@ import './container.css';
 import Image from './image';
 import Text from './text';
 
+const BgImage = (name) => {
+  return require(`./assets/${name}`)
+}
+
 function Container({ selectedCharacter, animate }) {
   if (!selectedCharacter) return null;
 
   return (
-    <div className="container">
+    <div className="container" style={{
+      backgroundImage: `url(${BgImage(selectedCharacter.Wallpaper)})`}}>
       <Image character={selectedCharacter} animate={animate} />
       <Text character={selectedCharacter} />
     </div>
