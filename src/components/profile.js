3e7e438c9f6de926c7110d93aaf8a036f71275ae
deryana <derana765@gmail.com>
2024-07-19 15:01:43 +0700

    update img

--- src/components/profile.js
+++ src/components/profile.js
@@ -9,25 +9,41 @@ function Profile() {
       <div className="profile-box">
         <div class="profile-section">
           <i class="fa-brands fa-github"></i>
-          <a href="https://github.com/deriana" target="blank" className="profile-link">
+          <a
+            href="https://github.com/deriana"
+            target="blank"
+            className="profile-link"
+          >
             <p>Github</p>
           </a>
         </div>
         <div class="profile-section">
           <i class="fa-brands fa-facebook"></i>
-          <a href="https://www.facebook.com/jerri.maruf" target="blank" className="profile-link">
+          <a
+            href="https://www.facebook.com/jerri.maruf"
+            target="blank"
+            className="profile-link"
+          >
             <p>Facebook</p>
           </a>
         </div>
         <div class="profile-section">
           <i class="fa-brands fa-instagram"></i>
-          <a href="https://www.instagram.com/hi_deri_/" target="blank" className="profile-link">
+          <a
+            href="https://www.instagram.com/hi_deri_/"
+            target="blank"
+            className="profile-link"
+          >
             <p>Instagram</p>
           </a>
         </div>
         <div class="profile-section">
           <i class="fa-brands fa-x-twitter"></i>
-          <a href="https://x.com/Deriana765" target="blank" className="profile-link">
+          <a
+            href="https://x.com/Deriana765"
+            target="blank"
+            className="profile-link"
+          >
             <p>Twitter</p>
           </a>
         </div>
